5585ecd97dfbf1cc18b7cce6bf76d5e894d12522
wonsub <secretfice@naver.com>
2017-05-10 01:13:59 +0900

    프로필 이미지, 굴러가는 돌 개선
    추가적인 스크립트 작성

---
+++ Object/SplitRock/kinematic_trigger.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kinematic_trigger : MonoBehaviour {
+
+
+    public GameObject gb;
+    Rigidbody[] rbs;
+    MeshCollider[] mcs;
+
+
+   
+
+	void Start()
+    {
+        rbs = gb.GetComponentsInChildren<Rigidbody>();
+        mcs = gb.GetComponentsInChildren<MeshCollider>();
+
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+   void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag != "Floor"){
+            StartCoroutine("Split");
+        }
+    }
+    IEnumerator Split()
+    {
+        foreach (Rigidbody rb in rbs)
+        {
+            rb.isKinematic = false;
+        }
+        foreach (MeshCollider mc in mcs)
+        {
+            mc.enabled = true;
+        }
+       
+        yield return new WaitForSeconds(0.5f);
+        gb.GetComponent<Rigidbody>().isKinematic = true;
+        yield return new WaitForSeconds(3.0f);
+        
+        StopCoroutine("Split");
+        DestroyObject(gb);
+       
+    }
+}
